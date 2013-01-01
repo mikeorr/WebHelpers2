2165dc02459d486d4c51cd91d29c3cec17da92ff
Mike Orr <sluggoster@gmail.com>
2013-01-01 12:06:51 -0800

    Delete Python 2.4 backport of ``defaultdict``.

--- webhelpers/containers.py
+++ webhelpers/containers.py
@@ -5,53 +5,10 @@ import a top-level module that's the same name as a module in the current
 package.
 """
 
+import collections
 import sys
 
 from webhelpers.misc import NotGiven
-
-try:
-    from collections import defaultdict
-except ImportError:   # Python < 2.5
-    class defaultdict(dict):
-        """Backport of Python 2.5's ``defaultdict``.
-
-        From the Python Cookbook.  Written by Jason Kirtland.
-        http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/523034
-
-        """
-        def __init__(self, default_factory=None, *a, **kw):
-            if (default_factory is not None and
-                not hasattr(default_factory, '__call__')):
-                raise TypeError('first argument must be callable')
-            dict.__init__(self, *a, **kw)
-            self.default_factory = default_factory
-        def __getitem__(self, key):
-            try:
-                return dict.__getitem__(self, key)
-            except KeyError:
-                return self.__missing__(key)
-        def __missing__(self, key):
-            if self.default_factory is None:
-                raise KeyError(key)
-            self[key] = value = self.default_factory()
-            return value
-        def __reduce__(self):
-            if self.default_factory is None:
-                args = tuple()
-            else:
-                args = self.default_factory,
-            return type(self), args, None, None, self.items()
-        def copy(self):
-            return self.__copy__()
-        def __copy__(self):
-            return type(self)(self.default_factory, self)
-        def __deepcopy__(self, memo):
-            import copy
-            return type(self)(self.default_factory,
-                              copy.deepcopy(self.items()))
-        def __repr__(self):
-            return 'defaultdict(%s, %s)' % (self.default_factory,
-                                            dict.__repr__(self))
 
 class DumbObject(object):
     """A container for arbitrary attributes.
@@ -102,7 +59,7 @@ class Counter(object):
     """
 
     def __init__(self):
-        self.result = defaultdict(int)
+        self.result = collections.defaultdict(int)
         self.total = 0  # Number of times instance has been called.
 
     def __call__(self, item):
@@ -174,7 +131,7 @@ class Accumulator(object):
     """
 
     def __init__(self):
-        self.result = defaultdict(list)
+        self.result = collections.defaultdict(list)
 
     def __call__(self, key, value):
         """Register a key-value pair."""
@@ -205,7 +162,7 @@ class UniqueAccumulator(object):
     """
 
     def __init__(self):
-        self.result = defaultdict(set)
+        self.result = collections.defaultdict(set)
 
     def __call__(self, key, value):
         """Register a key-value pair."""
