fdbb9a38c8d501baec9d2d5885d8b14398f2e29e
Mike Orr <sluggoster@gmail.com>
2013-01-01 16:16:10 -0800

    Delete routes dependency and webapp fixture.

--- tests/util.py
+++ tests/util.py
@@ -1,6 +1,4 @@
-from unittest import TestCase
-
-import routes
+from unittest import TestCase as WebHelpersTestCase
 
 def raises(exc, func, *args, **kw):
     try:
@@ -10,39 +8,3 @@ def raises(exc, func, *args, **kw):
     else:
         tup = func.__name__, e.__name__
         raise AssertionError("%s() did not raise %s" % tup)
-
-def test_environ():
-    return {
-        'HTTP_HOST': 'bob.local:5000',
-        'PATH_INFO': '/test',
-        'QUERY_STRING': 'test=webhelpers&framework=pylons',
-        'REQUEST_METHOD': 'GET',
-        'SERVER_NAME': '0.0.0.0',
-        'SCRIPT_NAME': '',
-        'pylons.environ_config': dict(session='test.session'),
-        'test.session': {},
-        'wsgi.multiprocess': False,
-        'wsgi.multithread': True,
-        'wsgi.run_once': False,
-        'wsgi.url_scheme': 'http'
-        }
-
-
-class WebHelpersTestCase(TestCase):
-    """Establishes a faux-environment for tests"""
-    def test_environ(self):
-        return {
-            'HTTP_HOST': 'bob.local:5000',
-            'PATH_INFO': '/test',
-            'QUERY_STRING': 'test=webhelpers&framework=pylons',
-            'REQUEST_METHOD': 'GET',
-            'SERVER_NAME': '0.0.0.0',
-            'SCRIPT_NAME': '',
-            'pylons.environ_config': dict(session='test.session'),
-            'test.session': {},
-            'wsgi.multiprocess': False,
-            'wsgi.multithread': True,
-            'wsgi.run_once': False,
-            'wsgi.url_scheme': 'http'
-            }
-
