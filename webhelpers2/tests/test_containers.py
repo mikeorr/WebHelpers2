4aedf3af0a5242e51ce076066a1ad70ecd3541b9
Mike Orr <sluggoster@gmail.com>
2013-08-04 23:22:58 -0700

    Port 'containers' tests to py.test.

--- webhelpers2/tests/test_containers.py
+++ webhelpers2/tests/test_containers.py
@@ -1,22 +1,70 @@
-# -*- coding: utf-8 -*-
-
-from nose.tools import eq_
-
-from util import raises
-
 from webhelpers2.containers import DumbObject
 from webhelpers2.containers import distribute
 
-def test_distribute():
-        food = ["apple", "banana", "carrot", "daikon", "egg", "fish", "gelato", "honey"]
-        eq_(distribute(food, 3, "H", ""), [["apple", "banana", "carrot"], ["daikon", "egg", "fish"], ["gelato", "honey", ""]])
-        eq_(distribute(food, 3, "V", ""), [["apple", "daikon", "gelato"], ["banana", "egg", "honey"], ["carrot", "fish", ""]])
-        eq_(distribute(food, 2, "H", ""), [["apple", "banana"], ["carrot", "daikon"], ["egg", "fish"], ["gelato", "honey"]])
-        eq_(distribute(food, 2, "V", ""), [["apple", "egg"], ["banana", "fish"], ["carrot", "gelato"], ["daikon", "honey"]])
+food = ["apple", "banana", "carrot", "daikon", "egg", "fish", "gelato", "honey"]
+food_extra = food + ["EXTRA"]
 
-def test_distribute_with_extra():
-        food = ["apple", "banana", "carrot", "daikon", "egg", "fish", "gelato", "honey", "EXTRA"]
-        eq_(distribute(food, 3, "H", ""), [["apple", "banana", "carrot"], ["daikon", "egg", "fish"], ["gelato", "honey", "EXTRA"]])
-        eq_(distribute(food, 3, "V", ""), [["apple", "daikon", "gelato"], ["banana", "egg", "honey"], ["carrot", "fish", "EXTRA"]])
-        eq_(distribute(food, 2, "H", ""), [["apple", "banana"], ["carrot", "daikon"], ["egg", "fish"], ["gelato", "honey"], ["EXTRA", ""]])
-        eq_(distribute(food, 2, "V", ""), [["apple", "fish"], ["banana", "gelato"], ["carrot", "honey"], ["daikon", "EXTRA"], ["egg", ""]])
+class TestDistribute(object):
+    def test1(self):
+        control = [
+            ["apple", "banana", "carrot"], 
+            ["daikon", "egg", "fish"], 
+            ["gelato", "honey", ""]]
+        assert distribute(food, 3, "H", "") == control
+
+    def test2(self):
+        control = [
+            ["apple", "daikon", "gelato"], 
+            ["banana", "egg", "honey"], 
+            ["carrot", "fish", ""]]
+        assert distribute(food, 3, "V", "") == control
+
+    def test3(self):
+        control = [
+            ["apple", "banana"], 
+            ["carrot", "daikon"], 
+            ["egg", "fish"], 
+            ["gelato", "honey"]]
+        assert distribute(food, 2, "H", "") == control
+
+    def test4(self):
+        control = [
+            ["apple", "egg"], 
+            ["banana", "fish"], 
+            ["carrot", "gelato"], 
+            ["daikon", "honey"]]
+        assert distribute(food, 2, "V", "") == control
+
+
+class TestDistributeWithExtra(object):
+    def test1(self):
+        control = [
+            ["apple", "banana", "carrot"], 
+            ["daikon", "egg", "fish"], 
+            ["gelato", "honey", "EXTRA"]]
+        assert distribute(food_extra, 3, "H", "") == control
+
+    def test2(self):
+        control = [
+            ["apple", "daikon", "gelato"], 
+            ["banana", "egg", "honey"], 
+            ["carrot", "fish", "EXTRA"]]
+        assert distribute(food_extra, 3, "V", "") == control
+
+    def test3(self):
+        control = [
+            ["apple", "banana"], 
+            ["carrot", "daikon"], 
+            ["egg", "fish"], 
+            ["gelato", "honey"], 
+            ["EXTRA", ""]]
+        assert distribute(food_extra, 2, "H", "") == control
+
+    def test4(self):
+        control = [
+            ["apple", "fish"], 
+            ["banana", "gelato"], 
+            ["carrot", "honey"], 
+            ["daikon", "EXTRA"], 
+            ["egg", ""]]
+        assert distribute(food_extra, 2, "V", "") == control
