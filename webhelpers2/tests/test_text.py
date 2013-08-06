0e383ad2ae09a9a45d27914594497eaf2c067abf
Mike Orr <sluggoster@gmail.com>
2013-08-05 22:45:59 -0700

    Port 'text' tests to py.test.

--- webhelpers2/tests/test_text.py
+++ webhelpers2/tests/test_text.py
@@ -1,45 +1,36 @@
 # -*- coding: utf-8 -*-
-from util import WebHelpersTestCase
-import unittest
-
-from nose.tools import eq_
-
 from webhelpers2.text import *
 
-class TestTextHelper(WebHelpersTestCase):
+class TestTextHelper(object):
     
     def test_excerpt(self):
-        self.assertEqual("...lo my wo...",
-                         excerpt("hello my world", "my", 3))
-        self.assertEqual("...is a beautiful morn...",
-                         excerpt("This is a beautiful morning", "beautiful", 5))
-        self.assertEqual("This is a...",
-                         excerpt("This is a beautiful morning", "this", 5))
-        self.assertEqual("...iful morning",
-                         excerpt("This is a beautiful morning", "morning", 5))
-        self.assertEqual("",
-                         excerpt("This is a beautiful morning", "day"))
+        assert "...lo my wo..." == excerpt("hello my world", "my", 3)
+        assert "...is a beautiful morn..." == \
+            excerpt("This is a beautiful morning", "beautiful", 5)
+        assert "This is a..." == excerpt("This is a beautiful morning", "this", 5)
+        assert "...iful morning" == excerpt("This is a beautiful morning", "morning", 5)
+        assert "" == excerpt("This is a beautiful morning", "day")
 
     def test_excerpt_with_regex(self):
-        self.assertEqual("...is a beautiful! mor...",
-                         excerpt("This is a beautiful! morning", "beautiful", 5))
-        self.assertEqual("...is a beautiful? mor...",
-                         excerpt("This is a beautiful? morning", "beautiful", 5))
+        assert "...is a beautiful! mor..." == \
+             excerpt("This is a beautiful! morning", "beautiful", 5)
+        assert "...is a beautiful? mor..." == \
+             excerpt("This is a beautiful? morning", "beautiful", 5)
 
     def test_excerpt_with_utf8(self):
-        self.assertEqual(u"...ﬃciency could not be ...",
-                         excerpt(u"That's why eﬃciency could not be helped", "could", 8))
+        assert u"...ﬃciency could not be ..." == \
+             excerpt(u"That's why eﬃciency could not be helped", "could", 8)
 
 
     def test_truncate(self):
-        self.assertEqual("Hello World!", truncate("Hello World!", 12))
-        self.assertEqual("Hello Wor...", truncate("Hello World!!", 12))
-        self.assertEqual("Hello...", truncate("Hello World!!", 12, whole_word=True))
+        assert "Hello World!" == truncate("Hello World!", 12)
+        assert "Hello Wor..." == truncate("Hello World!!", 12)
+        assert "Hello..." == truncate("Hello World!!", 12, whole_word=True)
 
     def test_strip_leading_whitespace(self):
         s = "    def fn(x):\n        return x\n"
         control = "def fn(x):\nreturn x\n"
-        eq_(control, strip_leading_whitespace(s))
+        assert control == strip_leading_whitespace(s)
 
     # @@MO wrap_paragraphs untested.
 
