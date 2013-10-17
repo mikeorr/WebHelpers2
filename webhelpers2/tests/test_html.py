798cab416cd8a9b21404e63b0627246705b7f972
Mike Orr <sluggoster@gmail.com>
2013-10-16 20:29:22 -0700

    Port 'html.builder' tests to py.test.

--- webhelpers2/tests/test_html.py
+++ webhelpers2/tests/test_html.py
@@ -1,96 +1,96 @@
-from nose.tools import eq_
+from __future__ import unicode_literals
 
 from webhelpers2.html import literal, lit_sub, escape, HTML
 from webhelpers2.html.builder import _attr_decode
 
 def test_double_escape():
-    quoted = escape(u'This string is "quoted"')
-    eq_(quoted, u"This string is &#34;quoted&#34;")
+    quoted = escape('This string is "quoted"')
+    assert quoted == "This string is &#34;quoted&#34;"
     dbl_quoted = escape(quoted)
-    eq_(quoted, dbl_quoted)
+    assert quoted == dbl_quoted
 
 def test_literal():
-    lit = literal(u"This string <>")
-    other = literal(u"<other>")
-    eq_(u"This string <><other>", lit + other)
+    lit = literal("This string <>")
+    other = literal("<other>")
+    assert "This string <><other>" == lit + other
     assert type(lit + other) is literal
     
-    eq_(u"&#34;<other>", '"' + other)
-    eq_(u"<other>&#34;", other + '"')
+    assert "&#34;<other>" == '"' + other
+    assert "<other>&#34;" == other + '"'
     
     mod = literal("<%s>ello")
-    eq_(u"<&lt;H&gt;>ello", mod % "<H>")
+    assert "<&lt;H&gt;>ello" == mod % "<H>"
     assert type(mod % "<H>") is literal
-    eq_(HTML("<a>"), "&lt;a&gt;")
+    assert HTML("<a>") == "&lt;a&gt;"
     assert type(HTML("<a>")) is literal
 
 def test_literal_dict():
-    lit = literal(u"This string <>")
+    lit = literal("This string <>")
     unq = "This has <crap>"
     sub = literal("%s and %s")
-    eq_(u"This string <> and This has &lt;crap&gt;", sub % (lit, unq))
+    assert "This string <> and This has &lt;crap&gt;", sub % (lit == unq)
     
     sub = literal("%(lit)s and %(lit)r")
-    eq_(u"This string <> and literal(u&#39;This string &lt;&gt;&#39;)", sub % dict(lit=lit))
+    assert "This string <> and literal(u&#39;This string &lt;&gt;&#39;)" == sub % dict(lit=lit)
     sub = literal("%(unq)r and %(unq)s")
-    eq_(u"&#39;This has &lt;crap&gt;&#39; and This has &lt;crap&gt;", sub % dict(unq=unq))
+    assert "u&#39;This has &lt;crap&gt;&#39; and This has &lt;crap&gt;" == sub % dict(unq=unq)
 
 def test_literal_mul():
-    lit = literal(u"<>")
-    eq_(u"<><><>", lit * 3)
+    lit = literal("<>")
+    assert "<><><>" == lit * 3
     assert isinstance(lit*3, literal)
 
 def test_literal_join():
-    lit = literal(u"<>")
+    lit = literal("<>")
     assert isinstance(lit.join(["f", "a"]), literal)
-    eq_(u"f<>a", lit.join(("f", "a")))
+    assert "f<>a", lit.join(("f" == "a"))
 
 def test_literal_int():
-    lit = literal(u"<%i>")
-    eq_(u"<5>", lit % 5)
+    lit = literal("<%i>")
+    assert "<5>" == lit % 5
 
 def test_html():
     a = HTML.a(href='http://mostlysafe\" <tag', c="Bad <script> tag")
-    eq_(a, u'<a href="http://mostlysafe&#34; &lt;tag">Bad &lt;script&gt; tag</a>')
+    assert a == '<a href="http://mostlysafe&#34; &lt;tag">Bad &lt;script&gt; tag</a>'
     
     img = HTML.img(src="http://some/image.jpg")
-    eq_(img, u'<img src="http://some/image.jpg" />')
+    assert img == '<img src="http://some/image.jpg" />'
     
     br = HTML.br()
-    eq_(u"<br />", br)
+    assert "<br />" == br
 
 def test_lit_re():
     lit = literal("This is a <string>")
     unlit = "This is also a <string>"
     
     result = lit_sub(r"<str", literal("<b"), lit)
-    eq_(u"This is a <bing>", escape(result))
+    assert "This is a <bing>" == escape(result)
     
     result = lit_sub(r"a <str", "a <b> <b", unlit)
-    eq_(u"This is also a &lt;b&gt; &lt;bing&gt;", escape(result))
+    assert "This is also a &lt;b&gt; &lt;bing&gt;" == escape(result)
 
 def test_unclosed_tag():
     result = HTML.form(_closed=False)
     print result
-    eq_(u"<form>", result)
+    assert "<form>" == result
     
     result = HTML.form(_closed=False, action="hello")
-    eq_(u'<form action="hello">', result)
+    assert '<form action="hello">' == result
 
 def test_newline_arg():
-    eq_(HTML.a(),         literal(u"<a></a>"))
-    eq_(HTML.a(_nl=True), literal(u"<a>\n</a>\n"))
-    eq_(HTML.a(_closed=False),           literal(u"<a>"))
-    eq_(HTML.a(_closed=False, _nl=True), literal(u"<a>\n"))
-    eq_(HTML.a("A", "B", href="/"),      literal(u'<a href="/">AB</a>'))
-    eq_(HTML.a("A", "B", href="/", _nl=True), literal(u'<a href="/">\nA\nB\n</a>\n'))
+    assert HTML.a() ==         literal("<a></a>")
+    assert HTML.a(_nl=True) == literal("<a>\n</a>\n")
+    assert HTML.a(_closed=False) ==           literal("<a>")
+    assert HTML.a(_closed=False, _nl=True) == literal("<a>\n")
+    assert HTML.a("A", "B", href="/") ==      literal('<a href="/">AB</a>')
+    assert HTML.a("A", "B", href="/", _nl=True) == literal('<a href="/">\nA\nB\n</a>\n')
 
 def test_attr_decode():
-    eq_(_attr_decode("foo"),   "foo")
-    eq_(_attr_decode("class_"),   "class")
-    eq_(_attr_decode("data_foo"), "data-foo")
-    eq_(_attr_decode("_data_foo_bar_"), "-data-foo-bar")
-    eq_(_attr_decode("_data_foo_bar_"), "-data-foo-bar")
+    assert _attr_decode("foo") ==   "foo"
+    assert _attr_decode("class_") ==   "class"
+    assert _attr_decode("data_foo") == "data-foo"
+    assert _attr_decode("_data_foo_bar_") == "-data-foo-bar"
+    assert _attr_decode("_data_foo_bar_") == "-data-foo-bar"
 
 def test_tag_with_data_attr():
-    eq_(HTML.span(data_foo="bar"), literal(u'<span data-foo="bar"></span>'))
+    assert HTML.span(data_foo="bar") == literal('<span data-foo="bar"></span>')
--- webhelpers2/tests/test_html_attrs.py
+++ webhelpers2/tests/test_html_attrs.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
 
-from nose.tools import eq_
-
 from webhelpers2.html import literal, escape, HTML
 from webhelpers2.html.builder import _attr_decode
 from webhelpers2.html.builder import format_attrs
@@ -9,21 +7,21 @@ from webhelpers2.html.builder import format_attrs
 def test_style_arg1():
     style = ["margin:0", "padding: 0"]
     control = literal(' style="margin:0; padding: 0"')
-    eq_(format_attrs(style=style), control)
+    assert format_attrs(style=style) == control
 
 def test_style_arg2():
     style = ["margin:0", "padding: 0"]
     control = literal(' maxwidth="10" style="margin:0; padding: 0"')
-    eq_(format_attrs(style=style, maxwidth=10), control)
+    assert format_attrs(style=style, maxwidth=10) == control
     
 def test_class_arg1():
     class_ = ["foo", "bar"]
     control = literal(' class="foo bar"')
-    eq_(format_attrs(class_=class_), control)
+    assert format_attrs(class_=class_) == control
 
 def test_class_arg2():
     class_ = ["foo", "bar"]
     args = {"class_": class_, "class": "baz"}
     control = literal(' class="foo bar"')
-    eq_(format_attrs(**args), control)
+    assert format_attrs(**args) == control
     
