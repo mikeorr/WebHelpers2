4ec9d5565550d57c11ff457e323e94006af5f7b4
Mike Orr <sluggoster@gmail.com>
2013-10-28 21:20:51 -0700

    Split multiple asserts to separate tests.

--- webhelpers2/tests/test_text.py
+++ webhelpers2/tests/test_text.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
 from webhelpers2.text import *
 
-class TestTextHelper(object):
-    
+class TestExcerptHelper(object):
     def test_excerpt(self):
         assert "...lo my wo..." == excerpt("hello my world", "my", 3)
+
+    def test_excerpt2(self):
         assert "...is a beautiful morn..." == \
             excerpt("This is a beautiful morning", "beautiful", 5)
+
+    def test_excerpt3(self):
         assert "This is a..." == excerpt("This is a beautiful morning", "this", 5)
+
+    def test_excerpt4(self):
         assert "...iful morning" == excerpt("This is a beautiful morning", "morning", 5)
+
+    def test_excerpt5(self):
         assert "" == excerpt("This is a beautiful morning", "day")
 
     def test_excerpt_with_regex(self):
         assert "...is a beautiful! mor..." == \
              excerpt("This is a beautiful! morning", "beautiful", 5)
+
+    def test_excerpt_with_regex2(self):
         assert "...is a beautiful? mor..." == \
              excerpt("This is a beautiful? morning", "beautiful", 5)
 
@@ -22,18 +31,28 @@ class TestTextHelper(object):
              excerpt(u"That's why eﬃciency could not be helped", "could", 8)
 
 
+class TestTruncateHelper(object):
     def test_truncate(self):
         assert "Hello World!" == truncate("Hello World!", 12)
+
+    def test_truncate2(self):
         assert "Hello Wor..." == truncate("Hello World!!", 12)
+
+    def test_truncate3(self):
         assert "Hello..." == truncate("Hello World!!", 12, whole_word=True)
 
+
+class TestStripLeadingWhitespaceHelper(object):
     def test_strip_leading_whitespace(self):
         s = "    def fn(x):\n        return x\n"
         control = "def fn(x):\nreturn x\n"
         assert control == strip_leading_whitespace(s)
 
-    # @@MO wrap_paragraphs untested.
 
+# @@MO wrap_paragraphs untested.
+
+
+class TestURLifyHelper(object):
     def test_urlify(self):
         s = "What is this? It is a car."
         control = "what-is-this%3F-it-is-a-car."
